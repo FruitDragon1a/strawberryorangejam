74c157738c7b602bb7b802a3464b23a5676e98b6
TomatoRadio <133932651+TomatoRadio@users.noreply.github.com>
2026-01-03 19:31:26 -0500

    DI

--- js/plugins/FWD_Eventpoints.js
+++ js/plugins/FWD_Eventpoints.js
@@ -1,6 +1,6 @@
 /*:
 @author FoGsesipod, WHITENOISE, Draughtyan
-@plugindesc v1.0.2 Add event points to audio files for event triggering.
+@plugindesc v1.0.4 Add event points to audio files for event triggering.
 @help
 This plugin allows you to add 'PING' points to audio files, which can be used to trigger events.
 To add a PING point, you need to modify the audio file's metadata. You can use a tool like Audacity to add custom metadata tags.
@@ -23,15 +23,32 @@ You can use the following plugin commands in your events:
 
 You can also add ping listeners in your code using the following functions:
 FWD.AddPingListener(pingname, callback): Adds a listener for the specified ping point. If pingname is null, undefined, empty, or 'anonymous', it adds a listener for anonymous ping points.
+NOTE: PING LISTENERS DO NOT SUPPORT CUSTOM EPSILON VALUES
 
 Also available for script conditions or other uses:
 FWD.IsPingPointActive(pingname): Returns true if the specified ping point is currently active.
 FWD.IsAnonymousPingPointActive(): Returns true if any anonymous ping point is currently active.
 FWD.IsAnyPingPointActive(): Returns true if any ping point (named or anonymous) is currently active.
 
+Advanced Usage:
+Most functions support an optional second parameter 'E' which allows you to specify a custom epsilon value for determining if a ping point is active.
+By default, the epsilon value is set to 0.05 seconds. You most likely won't need to change this.
+
+Syntax Examples:
+FWD.IsPingPointActive("namehere", 0.1)
+FWD.IsAnonymousPingPointActive(0.02)
+FWD.IsAnyPingPointActive(0.2)
+
+Plugin command syntax examples for Wait command with custom epsilon:
+Wait for PingPoint any 0.1
+Wait for PingPoint Intro 0.02
+Wait for PingPoint anonymouspingpoint 0.2
+
 
 == Changelog ==
 v1.0.2 - Add support for multiple named ping points with the same identifier.
+v1.0.3 - Add custom epsilon support
+v1.0.4 - Fix bug where size of metadata was not being reported correctly, causing ping points to not be read.
 
 === License ===
 This plugin is licensed under the MIT License.
@@ -41,31 +58,22 @@ This plugin is licensed under the MIT License.
     const FWD = window.FWD || {};
     window.FWD = FWD;
 
-    FWD.pastPoint = false;
-
-    const ExtendedChecks = ["audio/me/prl_credits_ch1.ogg"];
-    const selectBuffer = function() {
-        if (AudioManager._meBuffer && AudioManager._meBuffer.isPlaying()) return AudioManager._meBuffer;
-        return AudioManager._bgmBuffer;
-    }
-
+    const SEARCH_RANGE = 40;
     const old_readMetaData = WebAudio.prototype._readMetaData;
     WebAudio.prototype._readMetaData = function (array, index, size) {
-        //console.log(array,index,size);
-        if (ExtendedChecks.includes(this._url)) size = 700;
         this._PingPoints = {};
         this._TriggeredPingPoints = {};
         this._PingListeners = {};
         // this._AnonymousPingPoints = [];
         // this._AnonymousPointTriggered = false;
-        for (let i = index; i < index + size - 10; i++) {
-            //console.log(i);
+        let charsSinceLastPing = -size;
+        let i = index
+        while (charsSinceLastPing < SEARCH_RANGE) {
             if (this._readFourCharacters(array, i) === 'PING') {
                 let text = '';
                 while (array[i] > 0) {
                     text += String.fromCharCode(array[i++]);
                 }
-                console.log(text);
                 if (text.match(/PING([A-Za-z0-9]+)?=([0-9]+)/)) {
                     let pingname = RegExp.$1 || "__ANONYMOUS__";
                     let pingsamplepos = RegExp.$2;
@@ -77,24 +85,26 @@ This plugin is licensed under the MIT License.
                         this._TriggeredPingPoints[pingname] = false;
                     }
                 }
+            } else {
+                charsSinceLastPing++;
             }
+            i++;
+
         }
-        //console.log(this);
         old_readMetaData.call(this, array, index, size);
     }
 
     const EPSILON = 0.05
-    FWD.IsAnyPingPointActive = function () {
-        let buffer = selectBuffer();
+    FWD.IsAnyPingPointActive = function (E = EPSILON) {
+        let buffer = AudioManager._bgmBuffer;
         if (!buffer) return false;
         if (!buffer.isPlaying()) return false;
         let currentTime = buffer.seek();
 
-        for (let pinglist of buffer._PingPoints) {
-            for (let point of pinglist) {
-                if (FWD.pastPoint) return currentTime >= point;
+        for (let pingname in buffer._PingPoints) {
+            for (let point of buffer._PingPoints[pingname]) {
                 let diff = Math.abs(point - currentTime);
-                if (diff <= EPSILON && diff >= 0) {
+                if (diff <= E && diff >= 0) {
                     return true;
                 }
             }
@@ -104,23 +114,25 @@ This plugin is licensed under the MIT License.
     }
     window.BangingYroueMom = FWD.IsAnyPingPointActive; // legacy name
 
-    FWD.IsPingPointActive = function (pingname) {
-        let buffer = selectBuffer();
+    FWD.IsPingPointActive = function (pingname, E = EPSILON) {
+        let buffer = AudioManager._bgmBuffer;
         if (!buffer) return false;
         if (!buffer.isPlaying()) return false;
+        if (E !== EPSILON) { // only default EPSILON gets cached, so if custom E is used, do full check
+            return FWD._IsPingPointActive(pingname, E);
+        }
         return buffer._TriggeredPingPoints[pingname] === true;
     }
-    FWD._IsPingPointActive = function (pingname) {
-        let buffer = selectBuffer();
+    FWD._IsPingPointActive = function (pingname, E = EPSILON) {
+        let buffer = AudioManager._bgmBuffer;
         if (!buffer) return false;
         if (!buffer.isPlaying()) return false;
         let currentTime = buffer.seek();
 
         if (pingname in buffer._PingPoints) {
             for (let point of buffer._PingPoints[pingname]) {
-                if (FWD.pastPoint) return currentTime >= point;
                 let diff = Math.abs(point - currentTime);
-                if (diff <= EPSILON && diff >= 0) {
+                if (diff <= E && diff >= 0) {
                     return true;
                 }
             }
@@ -128,18 +140,21 @@ This plugin is licensed under the MIT License.
         return false;
     }
 
-    FWD.IsAnonymousPingPointActive = function () {
-        let buffer = selectBuffer();
+    FWD.IsAnonymousPingPointActive = function (E = EPSILON) {
+        let buffer = AudioManager._bgmBuffer;
         if (!buffer) return false;
         if (!buffer.isPlaying()) return false;
+        if (E !== EPSILON) {
+            return FWD._IsPingPointActive('__ANONYMOUS__', E);
+        }
         return buffer._TriggeredPingPoints['__ANONYMOUS__'] === true;
     }
-    FWD._IsAnonymousPingPointActive = function () {
-        return FWD._IsPingPointActive('__ANONYMOUS__');
+    FWD._IsAnonymousPingPointActive = function (E = EPSILON) {
+        return FWD._IsPingPointActive('__ANONYMOUS__', E);
     }
 
     FWD.AddPingListener = function (pingname, callback) {
-        let buffer = selectBuffer();
+        let buffer = AudioManager._bgmBuffer;
         if (!buffer) return;
 
         if (pingname === null || pingname === undefined || pingname === '' || pingname.toLowerCase() === 'anonymous') {
@@ -156,7 +171,7 @@ This plugin is licensed under the MIT License.
     const old_updateWebAudio = SceneManager.updateWebAudio;
     SceneManager.updateWebAudio = function () {
         old_updateWebAudio.call(this);
-        let buffer = selectBuffer();
+        let buffer = AudioManager._bgmBuffer;
         if (!buffer) return;
         if (!buffer.isPlaying()) return;
 
@@ -179,9 +194,14 @@ This plugin is licensed under the MIT License.
             if (args[0].trim().toLowerCase() === 'for') {
                 if (args[1].trim().toLowerCase() === 'pingpoint' || args[1].trim().toLowerCase() === 'ping') {
                     let pingname = '';
+                    let epsilon = undefined
                     if (args[2]) {
                         pingname = args[2].trim();
                     }
+                    if (args[3]) {
+                        epsilon = parseFloat(args[3]);
+                    }
+                    this._waitEpsilon = epsilon;
                     if (pingname === '') {
                         this._waitMode = 'anonymouspingpoint';
                     } else if (pingname === 'any' || pingname === 'all') {
@@ -198,21 +218,24 @@ This plugin is licensed under the MIT License.
     const old_gameInterpreter_updateWaitMode = Game_Interpreter.prototype.updateWaitMode;
     Game_Interpreter.prototype.updateWaitMode = function () {
         if (this._waitMode === 'pingpoint') {
-            if (FWD.IsPingPointActive(this._waitPingPointName)) {
+            if (FWD.IsPingPointActive(this._waitPingPointName, this._waitEpsilon)) {
                 this._waitMode = '';
                 this._waitPingPointName = '';
+                this._waitEpsilon = undefined;
             } else {
                 return true;
             }
         } else if (this._waitMode === 'anonymouspingpoint') {
-            if (FWD.IsAnonymousPingPointActive()) {
+            if (FWD.IsAnonymousPingPointActive(this._waitEpsilon)) {
                 this._waitMode = '';
+                this._waitEpsilon = undefined;
             } else {
                 return true;
             }
         } else if (this._waitMode === 'pingpointany') {
-            if (FWD.IsAnyPingPointActive()) {
+            if (FWD.IsAnyPingPointActive(this._waitEpsilon)) {
                 this._waitMode = '';
+                this._waitEpsilon = undefined;
             } else {
                 return true;
             }
