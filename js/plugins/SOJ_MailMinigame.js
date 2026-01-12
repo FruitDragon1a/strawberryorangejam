1cf9a50d3ce4e2534963965d0e5d70396ceb96ad
FruitDragon <134797844+FruitDragon1a@users.noreply.github.com>
2026-01-11 19:25:13 -0800

    work on mail minigame

--- js/plugins/SOJ_MailMinigame.js
+++ js/plugins/SOJ_MailMinigame.js
@@ -1,8 +1,21 @@
 SOJ = SOJ || {};
 SOJ.MM = SOJ.MM || {};
 SOJ.MM.EH = LanguageManager.getTextData("soj_minigame_mailman","EligibleHouses")
-console.log(SOJ.MM.EH)
+SOJ.MM.PickedHouses = []
 
-Game_Interpreter.prototype.pickHouses = function() {
+Game_Interpreter.prototype.pickHouse = function() {
+    num = Math.randomInt(13) + 1
+    while (SOJ.MM.PickedHouses.contains(num)) {
+        num = Math.randomInt(13) + 1
+    }
+    SOJ.MM.PickedHouses.push(num)
+    this.pickRecipient(SOJ.MM.EH[`${num}`])
+}
 
+Game_Interpreter.prototype.pickRecipient = function(HouseData) {
+    num = Math.randomInt(HouseData.length) + 1
+    RecipientData = HouseData[`${num}`]
+    $gameVariables.setValue(1965,RecipientData.recipient)
+    $gameVariables.setValue(1966,RecipientData.sender)
+    $gameVariables.setValue(1967,RecipientData.what)
 }
