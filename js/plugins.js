8b4dfb90dc58080fbf1ca6fc4a956bd21553c7a8
FruitDragon <134797844+FruitDragon1a@users.noreply.github.com>
2026-01-11 17:41:35 -0800

    mailman minigame stuff

--- js/plugins.js
+++ js/plugins.js
@@ -199,5 +199,6 @@ var $plugins =
 {"name":"SOJ_SLPCompat","status":true,"description":"","parameters":{}},
 {"name":"SOJ_Base","status":true,"description":"v1.0","parameters":{}},
 {"name":"TRain_TextEval","status":true,"description":"Execute between text & Centering","parameters":{}},
-{"name":"SOJ_BasketballWindows","status":true,"description":"","parameters":{}}
+{"name":"SOJ_BasketballWindows","status":true,"description":"","parameters":{}},
+{"name":"SOJ_MailMinigame","status":true,"description":"","parameters":{}}
 ];
---
+++ js/plugins/SOJ_MailMinigame.js
@@ -0,0 +1,8 @@
+SOJ = SOJ || {};
+SOJ.MM = SOJ.MM || {};
+SOJ.MM.EH = LanguageManager.getTextData("soj_minigame_mailman","EligibleHouses")
+console.log(SOJ.MM.EH)
+
+Game_Interpreter.prototype.pickHouses = function() {
+
+}
